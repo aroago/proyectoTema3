574542d06ffbfb09400ab00fac7e378efc85d602
aroa granero <aroa.graoma@educa.jcyl.es>
2021-11-09 11:57:48 +0100

    day 09/11/2021 modify indexTema3

--- indexProyectoTema3.php
+++ indexProyectoTema3.php
@@ -234,7 +234,7 @@ Fecha Modificacion: 13/10/2021 -->
                 </tr>
                 <tr>
                     <td>25. Trabajar en PlantillaFormulario.php mi plantilla para hacer formularios como churros.</td>
-                    <td class="no_hecho">
+                    <td class="hecho">
                         <a href="./codigoPHP/ejercicio25.php"><img class="img" alt="Play" src="./webroot/img/play.png" /></a></td>
                     <td>
                         <a href="./mostrarcodigo/ejercicio25.php"> <img class="img" alt="Codigo" src="./webroot/img/codigo.png" /></a>
