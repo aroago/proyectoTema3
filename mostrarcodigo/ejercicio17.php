c90d26f17e84c6801c26d653b64eaf42fef02eec
aroa granero <aroa.graoma@educa.jcyl.es>
2021-11-26 10:10:50 +0100

    day 26/11/2021 modify ej17

---
+++ mostrarcodigo/ejercicio17.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<!--Aroa Granero Omañas 
+Fecha Creacion: 26/11/2021
+Fecha Modificacion: 26/11/2021 -->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="author" content="aroaGraneroOmañas">
+        <title>ejercicio 17</title>
+    </head>
+    <body>
+        <?php
+            highlight_file("../codigoPHP/ejercicio17.php");
+        ?>
+    </body>
+</html>
